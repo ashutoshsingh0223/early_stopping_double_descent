b90836f1120db4aa92e180dbc4193ac1087b8e8f
Ashutosh Singh <ashutosh.singh.de@gmail.com>
2021-12-04 21:36:04 +0100

    Add resnet code and resnet training script

--- model_select.py
+++ model_select.py
@@ -1,10 +1,12 @@
 from models import *
+import torch.nn as nn
 
 class BaseModel():
     @classmethod
     def create(cls, message_type = 'resnet', **kwargs):
         MESSAGE_TYPE_TO_CLASS_MAP = {
             'mcnn' : mcnn.make_cnn,
+            'resent': ResNet18
         }
 
         if message_type not in MESSAGE_TYPE_TO_CLASS_MAP:
